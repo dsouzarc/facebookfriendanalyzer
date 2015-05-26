52e2d154d2de5a2a7c362fcb0bce27752e35d9a1
Ryan D'souza <dsouzarc@gmail.com>
2015-05-26 13:43:56 -0400

    First commit

---
+++ Facebook Friend Analyzer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Facebook Friend Analyzer
+//
+//  Created by Ryan D'souza on 5/26/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Facebook Friend Analyzer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Facebook Friend Analyzer
+//
+//  Created by Ryan D'souza on 5/26/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
