e52fa9b4383c82d9df2d3a097af46068b2fb23fc
Ryan D'souza <dsouzarc@gmail.com>
2015-05-26 13:59:02 -0400

    Basic login with FB

---
+++ Facebook Friend Analyzer/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Facebook Friend Analyzer
+//
+//  Created by Ryan D'souza on 5/26/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface LoginViewController : ViewController
+
+@end
---
+++ Facebook Friend Analyzer/LoginViewController.m
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.m
+//  Facebook Friend Analyzer
+//
+//  Created by Ryan D'souza on 5/26/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
