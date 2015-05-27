837f266bd20578a921e4741092cc8398cca2b489
Ryan D'souza <dsouzarc@gmail.com>
2015-05-26 20:23:47 -0400

    Recursively gets a list of all Facebook friends and adds them to a Set

--- Facebook Friend Analyzer/ChooseAnalyzerViewController.m
+++ Facebook Friend Analyzer/ChooseAnalyzerViewController.m
@@ -7,31 +7,92 @@
 //
 
 #import "ChooseAnalyzerViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @interface ChooseAnalyzerViewController ()
+
+@property (strong, nonatomic) NSMutableSet *people;
 
 @end
 
 @implementation ChooseAnalyzerViewController
 
+- (instancetype) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    self.people = [[NSMutableSet alloc] init];
+    
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    [self getFacebookFriends];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) getFacebookFriends
+{
+    //GET LIST OF FRIENDS
+    NSString *urlRequest = @"/me/taggable_friends?fields=name,picture.width(300),limit=500";
+    
+    [[[FBSDKGraphRequest alloc] initWithGraphPath:urlRequest parameters:nil] startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+        if(error) {
+            NSLog(@"ERROR AT USER TAGGABLE");
+            NSLog(error.description);
+        }
+        
+        else {
+            NSDictionary *formattedResults = (NSDictionary*) result;
+            NSArray *people = [formattedResults objectForKey:@"data"];
+            NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
+            
+            [self addPeople:people];
+            [self recursivelyAddPeople:pagingInformation[@"next"]];
+        }
+    }];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void) recursivelyAddPeople:(NSString*)url
+{
+    NSLog(@"Still working... %ld", (long)self.people.count);
+    NSData *data = [[NSString stringWithContentsOfURL:[NSURL URLWithString:url] encoding:NSUTF8StringEncoding error:nil] dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSDictionary *formattedResults = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    
+    NSArray *people = [formattedResults objectForKey:@"data"];
+    [self addPeople:people];
+    NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
+    
+    if(pagingInformation && pagingInformation[@"next"]) {
+        [self recursivelyAddPeople:pagingInformation[@"next"]];
+    }
+    
+    else {
+        NSLog(@"Here..?");
+    }
 }
-*/
+
+- (void) addPeople:(NSArray*)people
+{
+    for(NSDictionary *person in people) {
+        
+        @try {
+            NSString *name = person[@"name"];
+            
+            if([self.people containsObject:name]) {
+                NSLog(@"ALREADY CONTAINED: %@", name);
+            }
+            else {
+                [self.people addObject:name];
+            }
+        }
+        @catch (NSException *exception) {
+            NSLog(@"EXCEPTION ADDING: %@", exception.reason);
+        }
+    }
+}
+
 
 @end
