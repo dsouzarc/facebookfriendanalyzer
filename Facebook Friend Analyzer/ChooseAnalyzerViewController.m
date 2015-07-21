c4c5275b519c80233f5904e1d74483bf40a8a2e7
Ryan D'souza <dsouzarc@gmail.com>
2015-07-20 23:52:09 -0400

    Successfully gets all likes and comments for picture + some code cleanup + object creation + debugging

--- Facebook Friend Analyzer/ChooseAnalyzerViewController.m
+++ Facebook Friend Analyzer/ChooseAnalyzerViewController.m
@@ -38,130 +38,13 @@
     
     //[self getFacebookFriends];
     //[self getFacebookPosts];
+    [self getFacebookPhotos];
 }
 
 
 /****************************************
- *       FACEBOOK PHOTOS
+ *       FACEBOOK POSTS
  ****************************************/
-
-# pragma mark - PHOTOS
-
-- (void) getFacebookPhotos
-{
-    [self getFacebookPhotos:@"uploaded"];
-    [self getFacebookPhotos:@"tagged"];
-}
-
-- (void) getFacebookPhotos:(NSString*)kindOfPhotos
-{
-    NSString *urlRequest = [NSString stringWithFormat:@"me/photos/%@?limit=700", kindOfPhotos];
-    
-    [[[FBSDKGraphRequest alloc] initWithGraphPath:urlRequest parameters:nil] startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
-        if(error) {
-            NSLog(@"ERROR AT USER PHOTOS\t%@", [error description]);
-        }
-        
-        else {
-            NSDictionary *formattedResults = (NSDictionary*) result;
-            
-            NSArray *photos = formattedResults[@"data"];
-            for(NSDictionary *photo in photos) {
-                
-                Post *post = [[Post alloc] initWithMessage:photo[@"name"] postID:photo[@"id"] time:photo[@"created_time"]];
-                
-                //Get sub comments
-                [self getFacebookCommentsWithFacebookPostOrCommentID:post.postID];
-            }
-            
-            NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
-            if(pagingInformation && pagingInformation[@"next"]) {
-                [self getFacebookPhotosWithURL:pagingInformation[@"next"]];
-            }
-        }
-    }];
-}
-
-- (void) getFacebookPhotosWithURL:(NSString*)photosURL
-{
-    NSData *data = [[NSString stringWithContentsOfURL:[NSURL URLWithString:photosURL] encoding:NSUTF8StringEncoding error:nil] dataUsingEncoding:NSUTF8StringEncoding];
-    NSDictionary *formattedResults = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-    
-    NSArray *photos = formattedResults[@"data"];
-    for(NSDictionary *photo in photos) {
-        Post *post = [[Post alloc] initWithMessage:photo[@"name"] postID:photo[@"id"] time:photo[@"created_time"]];
-    }
-    
-    NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
-    if(pagingInformation && pagingInformation[@"next"]) {
-        [self getFacebookPhotosWithURL:pagingInformation[@"next"]];
-    }
-    
-    else {
-        NSLog(@"Here..?");
-    }
-}
-
-
-/****************************************
- *       FACEBOOK COMMENTS
- ****************************************/
-
-# pragma mark - Comments
-
-- (void) getFacebookCommentsWithFacebookPostOrCommentID:(NSString*)postID
-{
-    NSString *urlRequest = [NSString stringWithFormat:@"%@/comments&limit=150", postID];
-    
-    [[[FBSDKGraphRequest alloc] initWithGraphPath:urlRequest parameters:nil] startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
-        if(error) {
-            NSLog(@"ERROR AT USER COMMENTS\t%@", [error description]);
-        }
-        
-        else {
-            NSDictionary *formattedResults = (NSDictionary*) result;
-            
-            NSArray *comments = formattedResults[@"data"];
-            for(NSDictionary *commentDict in comments) {
-                
-                Comment *comment = [[Comment alloc] initWithResponseDictionary:commentDict postID:postID];
-                
-                //Get sub comments
-                [self getFacebookCommentsWithFacebookPostOrCommentID:comment.commentID];
-            }
-            
-            NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
-            if(pagingInformation && pagingInformation[@"next"]) {
-                [self getFacebookCommentsWithURL:pagingInformation[@"next"] postID:postID];
-            }
-        }
-    }];
-}
-
-- (void) getFacebookCommentsWithURL:(NSString*)nextURL postID:(NSString*)postID
-{
-    NSData *data = [[NSString stringWithContentsOfURL:[NSURL URLWithString:nextURL] encoding:NSUTF8StringEncoding error:nil] dataUsingEncoding:NSUTF8StringEncoding];
-    NSDictionary *formattedResults = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-    
-    NSArray *comments = formattedResults[@"data"];
-    for(NSDictionary *commentDict in comments) {
-        Comment *comment = [[Comment alloc] initWithResponseDictionary:commentDict postID:postID];
-    }
-    
-    NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
-    if(pagingInformation && pagingInformation[@"next"]) {
-        [self getFacebookCommentsWithURL:pagingInformation[@"next"] postID:postID];
-    }
-    
-    else {
-        NSLog(@"Here..?");
-    }
-}
-
-
-/****************************************
-*       FACEBOOK POSTS
-****************************************/
 
 # pragma mark - Posts
 
@@ -180,16 +63,18 @@
             
             NSArray *postResults = formattedResults[@"data"];
             for(NSDictionary *dict in postResults) {
+                NSString *messageAndStory = [NSString stringWithFormat:@"Story: %@. Message: %@", dict[@"story"], dict[@"message"]];
+                Post *post = [[Post alloc] initWithMessage:messageAndStory postID:dict[@"id"] time:dict[@"created_time"]];
+                [self getPostLikesAndComments:post];
                 //NSLog(@"%@", dict[@"message"]);
             }
             
             NSDictionary *firstPost = postResults[1];
             NSString *postID = firstPost[@"id"];
             NSLog(@"Post ID: %@\t%@", postID, firstPost[@"message"]);
-            [self getFacebookCommentsWithFacebookPostOrCommentID:postID];
-   
+            
             NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
-            //[self recursivelyGetPosts:pagingInformation[@"next"]];
+            [self recursivelyGetPosts:pagingInformation[@"next"]];
         }
     }];
 }
@@ -203,11 +88,141 @@
     
     NSArray *postResults = formattedResults[@"data"];
     for(NSDictionary *dict in postResults) {
-        NSLog(@"%@", dict[@"message"]);
+        NSString *messageAndStory = [NSString stringWithFormat:@"Story: %@. Message: %@", dict[@"story"], dict[@"message"]];
+        Post *post = [[Post alloc] initWithMessage:messageAndStory postID:dict[@"id"] time:dict[@"created_time"]];
+        [self getPostLikesAndComments:post];
+        //NSLog(@"%@", dict[@"message"]);
     }
     
     if(pagingInformation && pagingInformation[@"next"]) {
-        //[self recursivelyGetPosts:pagingInformation[@"next"]];
+        [self recursivelyGetPosts:pagingInformation[@"next"]];
+    }
+    
+    else {
+        NSLog(@"Here..?");
+    }
+}
+
+- (void) getPostLikesAndComments:(Post*)post
+{
+    [self getFacebookLikesWithPostID:post.postID];
+    [self getFacebookCommentsWithFacebookPostOrCommentID:post.postID depthLevel:0];
+}
+
+
+/****************************************
+ *       FACEBOOK PHOTOS
+ ****************************************/
+
+# pragma mark - PHOTOS
+
+- (void) getFacebookPhotos
+{
+    [self getFacebookPhotos:@"uploaded"];
+    [self getFacebookPhotos:@"tagged"];
+}
+
+- (void) getFacebookPhotos:(NSString*)kindOfPhotos
+{
+    NSString *urlRequest = [NSString stringWithFormat:@"me/photos/%@?limit=50", kindOfPhotos];
+    
+    [[[FBSDKGraphRequest alloc] initWithGraphPath:urlRequest parameters:nil] startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+        if(error) {
+            NSLog(@"ERROR AT USER PHOTOS\t%@", [error description]);
+        }
+        
+        else {
+            NSDictionary *formattedResults = (NSDictionary*) result;
+            
+            NSArray *photos = formattedResults[@"data"];
+            for(NSDictionary *photo in photos) {
+                Post *post = [[Post alloc] initWithMessage:photo[@"name"] postID:photo[@"id"] time:photo[@"created_time"]];
+                [self getPostLikesAndComments:post];
+            }
+            
+            NSDictionary *photo = photos[0];
+            Post *post = [[Post alloc] initWithMessage:photo[@"name"] postID:photo[@"id"] time:photo[@"created_time"]];
+            //[self getPostLikesAndComments:post];
+            
+            NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
+            if(pagingInformation && pagingInformation[@"next"]) {
+                //[self getFacebookPhotosWithURL:pagingInformation[@"next"]];
+            }
+        }
+    }];
+}
+
+- (void) getFacebookPhotosWithURL:(NSString*)photosURL
+{
+    NSData *data = [[NSString stringWithContentsOfURL:[NSURL URLWithString:photosURL] encoding:NSUTF8StringEncoding error:nil] dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *formattedResults = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    
+    NSArray *photos = formattedResults[@"data"];
+    for(NSDictionary *photo in photos) {
+        Post *post = [[Post alloc] initWithMessage:photo[@"name"] postID:photo[@"id"] time:photo[@"created_time"]];
+        [self getPostLikesAndComments:post];
+    }
+    
+    NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
+    if(pagingInformation && pagingInformation[@"next"]) {
+        [self getFacebookPhotosWithURL:pagingInformation[@"next"]];
+    }
+    
+    else {
+        NSLog(@"Here..?");
+    }
+}
+
+/****************************************
+ *       FACEBOOK COMMENTS
+ ****************************************/
+
+# pragma mark - Comments
+
+- (void) getFacebookCommentsWithFacebookPostOrCommentID:(NSString*)postID depthLevel:(int)depthLevel
+{
+    NSString *urlRequest = [NSString stringWithFormat:@"%@/comments?limit=150", postID];
+    
+    [[[FBSDKGraphRequest alloc] initWithGraphPath:urlRequest parameters:nil] startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+        if(error) {
+            NSLog(@"ERROR AT USER COMMENTS\t%@", [error description]);
+        }
+        
+        else {
+            NSDictionary *formattedResults = (NSDictionary*) result;
+            NSArray *comments = formattedResults[@"data"];
+            
+            for(NSDictionary *commentDict in comments) {
+                Comment *comment = [[Comment alloc] initWithResponseDictionary:commentDict postID:postID];
+
+                //Get sub comments, but only if we're at the top level
+                if(depthLevel == 0) {
+                    [self getFacebookCommentsWithFacebookPostOrCommentID:comment.commentID depthLevel:1];
+                }
+            }
+            
+            NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
+            if(pagingInformation && pagingInformation[@"next"]) {
+                [self getFacebookCommentsWithURL:pagingInformation[@"next"] postID:postID];
+            }
+        }
+    }];
+}
+
+- (void) getFacebookCommentsWithURL:(NSString*)nextURL postID:(NSString*)postID
+{
+    NSData *data = [[NSString stringWithContentsOfURL:[NSURL URLWithString:nextURL] encoding:NSUTF8StringEncoding error:nil] dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *formattedResults = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    
+    NSArray *comments = formattedResults[@"data"];
+    for(NSDictionary *commentDict in comments) {
+        Comment *comment = [[Comment alloc] initWithResponseDictionary:commentDict postID:postID];
+        NSLog(@"Comment: %@", comment.message);
+    }
+    
+    NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
+    if(pagingInformation && pagingInformation[@"next"]) {
+        [self getFacebookCommentsWithURL:pagingInformation[@"next"] postID:postID];
     }
     
     else {
@@ -224,8 +239,7 @@
 
 - (void) getFacebookLikesWithPostID:(NSString*)postID
 {
-    NSString *urlRequest = [NSString stringWithFormat:@"%@/likes&limit=700", postID];
-    NSLog(@"URL: %@", urlRequest);
+    NSString *urlRequest = [NSString stringWithFormat:@"%@/likes?limit=700", postID];
     [[[FBSDKGraphRequest alloc] initWithGraphPath:urlRequest parameters:nil] startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
         if(error) {
             NSLog(@"ERROR AT USER LIKES\t%@", [error description]);
@@ -235,17 +249,22 @@
             NSDictionary *formattedResults = (NSDictionary*) result;
             NSArray *likeIDs = formattedResults[@"data"];
             
-            for(NSDictionary *like in likeIDs) {
-                NSLog(@"Liked it: %@\t%@", like[@"name"], like[@"id"]);
+
+            NSLog(@"LIKES: %@\t%ld", postID, (long)likeIDs.count);
+            
+            for(NSDictionary *likeID in likeIDs) {
+                Like *like = [[Like alloc] initWithPersonWhoLikedItID:likeID[@"id"] postID:postID];
             }
             
             NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
-            [self recursivelyGetFacebookLikesWithURL:pagingInformation[@"next"]];
+            if(pagingInformation && pagingInformation[@"next"]) {
+                [self recursivelyGetFacebookLikesWithURL:pagingInformation[@"next"] postID:postID];
+            }
         }
     }];
 }
 
-- (void) recursivelyGetFacebookLikesWithURL:(NSString*)urlRequest
+- (void) recursivelyGetFacebookLikesWithURL:(NSString*)urlRequest postID:(NSString*)postID
 {
     NSData *data = [[NSString stringWithContentsOfURL:[NSURL URLWithString:urlRequest] encoding:NSUTF8StringEncoding error:nil] dataUsingEncoding:NSUTF8StringEncoding];
     
@@ -254,12 +273,14 @@
     NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
     
     NSArray *likeResults = formattedResults[@"data"];
-    for(NSDictionary *dict in likeResults) {
-        NSLog(@"Liked it: %@\t%@", dict[@"name"], dict[@"id"]);
+    
+    NSLog(@"LIKES: %@\t%ld", postID, (long)likeResults.count);
+    for(NSDictionary *likeID in likeResults) {
+        Like *like = [[Like alloc] initWithPersonWhoLikedItID:likeID[@"id"] postID:postID];
     }
     
     if(pagingInformation && pagingInformation[@"next"]) {
-        [self recursivelyGetFacebookLikesWithURL:pagingInformation[@"next"]];
+        [self recursivelyGetFacebookLikesWithURL:pagingInformation[@"next"] postID:postID];
     }
     else {
         NSLog(@"No more next for FB URL..?");
@@ -286,11 +307,12 @@
         else {
             NSDictionary *formattedResults = (NSDictionary*) result;
             NSArray *people = [formattedResults objectForKey:@"data"];
+            [self addPeople:people];
             
             NSDictionary *pagingInformation = [formattedResults objectForKey:@"paging"];
-            
-            [self addPeople:people];
-            [self recursivelyAddPeople:pagingInformation[@"next"]];
+            if(pagingInformation && pagingInformation[@"next"]) {
+                [self recursivelyAddPeople:pagingInformation[@"next"]];
+            }
         }
     }];
 }
