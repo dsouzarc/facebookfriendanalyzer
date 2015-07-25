734eab95e3715b48134f549043f206b869b497d7
Ryan D'souza <dsouzarc@gmail.com>
2015-07-25 01:32:20 -0400

    Added new ViewController and updated UI

---
+++ Facebook Friend Analyzer/MainFriendAnalyzerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainFriendAnalyzerViewController.h
+//  Facebook Friend Analyzer
+//
+//  Created by Ryan D'souza on 7/25/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainFriendAnalyzerViewController : ViewController
+
+@end
