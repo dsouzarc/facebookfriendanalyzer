75b507b5e7846461a4459ddd802ba3f373046887
Ryan D'souza <dsouzarc@gmail.com>
2015-07-25 01:35:53 -0400

    Connected listeners

--- Facebook Friend Analyzer/MainFriendAnalyzerViewController.m
+++ Facebook Friend Analyzer/MainFriendAnalyzerViewController.m
@@ -19,19 +19,24 @@
     // Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)getFacebookFriends:(id)sender {
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)getFacebookPosts:(id)sender {
+    
 }
-*/
+
+- (IBAction)getFacebookPictures:(id)sender {
+    
+}
+
+- (IBAction)getFacebookVideos:(id)sender {
+    
+}
+
+- (IBAction)viewResults:(id)sender {
+    
+}
 
 @end
