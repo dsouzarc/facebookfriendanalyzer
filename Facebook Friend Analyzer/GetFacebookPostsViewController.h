3a9a9f3f718382eb56ffdfed7372343ab2f95597
Ryan D'souza <dsouzarc@gmail.com>
2015-07-27 01:43:57 -0400

    Set up UI

---
+++ Facebook Friend Analyzer/GetFacebookPostsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GetFacebookPostsViewController.h
+//  Facebook Friend Analyzer
+//
+//  Created by Ryan D'souza on 7/27/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface GetFacebookPostsViewController : ViewController
+
+@end
