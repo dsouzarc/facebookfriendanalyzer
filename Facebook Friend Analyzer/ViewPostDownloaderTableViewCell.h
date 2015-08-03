2b7f04c78fb061cab9fd02db2822f793840f49b5
Ryan D'souza <dsouzarc@gmail.com>
2015-08-03 01:22:23 -0400

    Added icons

---
+++ Facebook Friend Analyzer/ViewPostDownloaderTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ViewPostDownloaderTableViewCell.h
+//  Facebook Friend Analyzer
+//
+//  Created by Ryan D'souza on 8/3/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewPostDownloaderTableViewCell : UITableViewCell
+
+@end
---
+++ Facebook Friend Analyzer/ViewPostDownloaderTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ViewPostDownloaderTableViewCell.m
+//  Facebook Friend Analyzer
+//
+//  Created by Ryan D'souza on 8/3/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "ViewPostDownloaderTableViewCell.h"
+
+@implementation ViewPostDownloaderTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
