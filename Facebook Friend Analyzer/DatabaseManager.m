56f6412ae9b998fb72e4185863e2b035120e0df3
Ryan D'souza <dsouzarc@gmail.com>
2015-08-02 22:45:29 -0400

    Reformatted to use more effective sql practices - opens/closes db for each operation, uses finalize to remove sqlstatements

--- Facebook Friend Analyzer/DatabaseManager.m
+++ Facebook Friend Analyzer/DatabaseManager.m
@@ -8,9 +8,11 @@
 
 #import "DatabaseManager.h"
 
+static int MAX_DB_TRIES = 45;
+static float DB_SLEEP_FOR_TIME = 1.1;
+
 @interface DatabaseManager ()
 
-@property sqlite3 *database;
 @property (strong, nonatomic) NSString *databasePath;
 
 @end
@@ -44,14 +46,16 @@ static DatabaseManager *databaseManager = nil;
         NSArray *dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
         NSString *docsDir = dirPaths[0];
         self.databasePath = [[NSString alloc] initWithString:[docsDir stringByAppendingPathComponent: @"facebook.db"]];
+        
         const char *dbpath = [self.databasePath UTF8String];
+        sqlite3 *database;
         
         if ([[NSFileManager defaultManager] fileExistsAtPath: self.databasePath]) {
-            if (sqlite3_open(dbpath, &_database) == SQLITE_OK) {
+            if (sqlite3_open(dbpath, &database) == SQLITE_OK) {
                 NSLog(@"Database found and opened");
             }
             else {
-                NSLog(@"Failed to open database: %s", sqlite3_errmsg(_database));
+                NSLog(@"Failed to open database: %s", sqlite3_errmsg(database));
             }
         }
         else {
@@ -60,7 +64,7 @@ static DatabaseManager *databaseManager = nil;
                 NSLog(@"Error creating database: %@", error.description);
             }
             else {
-                if (sqlite3_open(dbpath, &_database) == SQLITE_OK) {
+                if (sqlite3_open(dbpath, &database) == SQLITE_OK) {
                     NSLog(@"Database created and opened");
                 }
                 else {
@@ -69,10 +73,12 @@ static DatabaseManager *databaseManager = nil;
             }
         }
         
-        [self createPersonTable];
-        [self createPostTable];
-        [self createLikeTable];
-        [self createCommentTable];
+        [self createPersonTable:database];
+        [self createPostTable:database];
+        [self createLikeTable:database];
+        [self createCommentTable:database];
+        
+        sqlite3_close(database);
     }
     
     return self;
@@ -89,18 +95,31 @@ static DatabaseManager *databaseManager = nil;
 {
     NSString *deleteAllSQL = @"DELETE FROM Person";
     
-    if(![self executeStatement:deleteAllSQL]) {
+    sqlite3 *database;
+    sqlite3_open([self.databasePath UTF8String], &database);
+    
+    if(![self executeStatement:deleteAllSQL database:database]) {
         NSLog(@"PROBLEM DELETING ALL PEOPLE");
     }
+    
+    sqlite3_close(database);
 }
 
-- (void) createPersonTable
+- (void) createPersonTable:(sqlite3*)database;
 {
     NSString *createPostSQL = @"create table if not exists Person(dbID integer primary key autoincrement, personID text, name text, profilePicture text)";
-    [self createTable:createPostSQL tableName:@"Person"];
+    [self createTable:createPostSQL tableName:@"Person" database:database];
 }
 
 - (void) addPersonToTable:(Person*)person
+{
+    sqlite3 *database;
+    sqlite3_open([self.databasePath UTF8String], &database);
+    [self addPersonToTable:person database:database];
+    sqlite3_close(database);
+}
+
+- (void) addPersonToTable:(Person *)person database:(sqlite3*)database
 {
     //Because ID can be null at times
     NSString *personID = person.id;
@@ -114,31 +133,36 @@ static DatabaseManager *databaseManager = nil;
     }
     
     NSString *insertSQL = [NSString stringWithFormat:@"insert into Person(personID, name, profilePicture) values ('%@', '%@', '%@')", person.id, name, person.profilePicture];
-    if(![self executeStatement:insertSQL]) {
+    if(![self executeStatement:insertSQL database:database]) {
         NSLog(@"PROBLEM INSERTING PERSON: %@\t%@", person.id, person.name);
     }
-    
 }
 
 - (void) addPeopleToDatabase:(NSArray*)people
 {
     char *errorMessage;
-    sqlite3_exec(self.database, "BEGIN TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3 *database;
+    sqlite3_open([self.databasePath UTF8String], &database);
+    
+    sqlite3_exec(database, "BEGIN TRANSACTION", NULL, NULL, &errorMessage);
     
     for(Person *person in people) {
-        [self addPersonToTable:person];
+        [self addPersonToTable:person database:database];
     }
-    sqlite3_exec(self.database, "COMMIT TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3_exec(database, "COMMIT TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3_close(database);
 }
 
 - (NSMutableArray*) getAllPeople
 {
     NSMutableArray *allPeople = [[NSMutableArray alloc] init];
     
+    sqlite3 *database;
+    sqlite3_open([self.databasePath UTF8String], &database);
     sqlite3_stmt *statement;
     char *allPeopleQuery = "SELECT * FROM Person";
     
-    if(sqlite3_prepare_v2(self.database, allPeopleQuery, -1, &statement, NULL) == SQLITE_OK) {
+    if(sqlite3_prepare_v2(database, allPeopleQuery, -1, &statement, NULL) == SQLITE_OK) {
         while(sqlite3_step(statement) == SQLITE_ROW) {
             NSString *personID = [NSString stringWithFormat:@"%s", sqlite3_column_text(statement, 1)];
             NSString *name = [NSString stringWithFormat:@"%s", sqlite3_column_text(statement, 2)];
@@ -151,10 +175,14 @@ static DatabaseManager *databaseManager = nil;
             Person *person = [[Person alloc] initWithID:personID name:name profilePicture:linkToProfPic];
             [allPeople addObject:person];
         }
+        
+        sqlite3_finalize(statement);
     }
     else {
-        printf("ERROR GETTING ALL PEOPLE FROM DB: %s\n", sqlite3_errmsg(self.database));
+        printf("ERROR GETTING ALL PEOPLE FROM DB: %s\n", sqlite3_errmsg(database));
     }
+    
+    sqlite3_close(database);
     
     return allPeople;
 }
@@ -166,29 +194,39 @@ static DatabaseManager *databaseManager = nil;
 
 # pragma mark - Likes
 
-- (void) createLikeTable
+- (void) createLikeTable:(sqlite3*)database
 {
     NSString *createPostSQL = @"create table if not exists Like(personWhoLikedItID integer primary key, postID text)";
-    [self createTable:createPostSQL tableName:@"Like"];
+    [self createTable:createPostSQL tableName:@"Like" database:database];
+}
+
+- (void) addLikeToTable:(Like *)like database:(sqlite3*)database
+{
+    NSString *insertSQL = [NSString stringWithFormat:@"insert into Like(personWhoLikedItID, postID) values ('%@', '%@')", like.personWhoLikedItID, like.postID];
+    if(![self executeStatement:insertSQL database:database]) {
+        NSLog(@"PROBLEM INSERTING LIKE: %@\t%@", like.personWhoLikedItID, like.postID);
+    }
 }
 
 - (void) addLikeToTable:(Like*)like
 {
-    NSString *insertSQL = [NSString stringWithFormat:@"insert into Like(personWhoLikedItID, postID) values ('%@', '%@')", like.personWhoLikedItID, like.postID];
-    if(![self executeStatement:insertSQL]) {
-        NSLog(@"PROBLEM INSERTING LIKE: %@\t%@", like.personWhoLikedItID, like.postID);
-    }
+    sqlite3 *database;
+    sqlite3_open([self.databasePath UTF8String], &database);
+    [self addLikeToTable:like database:database];
+    sqlite3_close(database);
 }
 
 - (void) addLikesToTable:(NSArray *)likes
 {
     char *errorMessage;
-    sqlite3_exec(self.database, "BEGIN TRANSACTION", NULL, NULL, &errorMessage);
-    
+    sqlite3 *database;
+    sqlite3_open([self.databasePath UTF8String], &database);
+    sqlite3_exec(database, "BEGIN TRANSACTION", NULL, NULL, &errorMessage);
     for(Like *like in likes) {
-        [self addLikeToTable:like];
+        [self addLikeToTable:like database:database];
     }
-    sqlite3_exec(self.database, "COMMIT TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3_exec(database, "COMMIT TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3_close(database);
 }
 
 
@@ -198,29 +236,41 @@ static DatabaseManager *databaseManager = nil;
 
 # pragma mark - Comments
 
-- (void) createCommentTable
+- (void) createCommentTable:(sqlite3*)database
 {
     NSString *createPostSQL = @"create table if not exists Comment(commentID integer primary key, message text, personID text, postID text, commentTime text)";
-    [self createTable:createPostSQL tableName:@"Comment"];
+    [self createTable:createPostSQL tableName:@"Comment" database:database];
+}
+
+- (void) addCommentToTable:(Comment *)comment database:(sqlite3*)database
+{
+    NSString *insertSQL = [NSString stringWithFormat:@"insert into Comment(commentID, message, personID, postID, commentTime) values ('%lld', '%@', '%@', '%@', '%@')", [comment.commentID longLongValue], comment.message, comment.personID, comment.postID, comment.commentTime];
+    if(![self executeStatement:insertSQL database:database]) {
+        NSLog(@"PROBLEM INSERTING COMMENT: %@\t%@", comment.commentID, comment.message);
+    }
 }
 
 - (void)addCommentToTable:(Comment*)comment
 {
-    NSString *insertSQL = [NSString stringWithFormat:@"insert into Comment(commentID, message, personID, postID, commentTime) values ('%lld', '%@', '%@', '%@', '%@')", [comment.commentID longLongValue], comment.message, comment.personID, comment.postID, comment.commentTime];
-    if(![self executeStatement:insertSQL]) {
-        NSLog(@"PROBLEM INSERTING COMMENT: %@\t%@", comment.commentID, comment.message);
-    }
+    sqlite3 *database;
+    sqlite3_open([self.databasePath UTF8String], &database);
+    [self addCommentToTable:comment database:database];
+    sqlite3_close(database);
+    
 }
 
 - (void) addCommentsToTable:(NSArray *)comments
 {
     char *errorMessage;
-    sqlite3_exec(self.database, "BEGIN TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3 *database;
+    sqlite3_open([self.databasePath UTF8String], &database);
     
+    sqlite3_exec(database, "BEGIN TRANSACTION", NULL, NULL, &errorMessage);
     for(Comment *comment in comments) {
-        [self addCommentToTable:comment];
+        [self addCommentToTable:comment database:database];
     }
-    sqlite3_exec(self.database, "COMMIT TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3_exec(database, "COMMIT TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3_close(database);
 }
 
 
@@ -230,29 +280,43 @@ static DatabaseManager *databaseManager = nil;
 
 # pragma mark - Posts
 
-- (void) createPostTable
+- (void) createPostTable:(sqlite3*)database
 {
     NSString *createPostSQL = @"create table if not exists Post(postID integer primary key, message text, time text)";
-    [self createTable:createPostSQL tableName:@"Post"];
+    [self createTable:createPostSQL tableName:@"Post" database:database];
+}
+
+- (void) addPostToTable:(Post *)post database:(sqlite3*)database
+{
+    NSString *insertSQL = [NSString stringWithFormat:@"insert into Post(postID, message, time) values ('%lld', '%@', '%@')",
+                           [post.postID longLongValue], post.message, post.time];
+    if(![self executeStatement:insertSQL database:database]) {
+        NSLog(@"PROBLEM INSERTING POST: %@\t%@", post.postID, post.message);
+    }
 }
 
 - (void)addPostToTable:(Post*)post
 {
-    NSString *insertSQL = [NSString stringWithFormat:@"insert into Post(postID, message, time) values ('%lld', '%@', '%@')", [post.postID longLongValue], post.message, post.time];
-    if(![self executeStatement:insertSQL]) {
-        NSLog(@"PROBLEM INSERTING POST: %@\t%@", post.postID, post.message);
-    }
+    sqlite3 *database;
+    sqlite3_open([self.databasePath UTF8String], &database);
+    
+    [self addPostToTable:post database:database];
+    sqlite3_close(database);
 }
 
 - (void) addPostsToTable:(NSArray *)posts
 {
     char *errorMessage;
-    sqlite3_exec(self.database, "BEGIN TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3 *database;
+    sqlite3_open([self.databasePath UTF8String], &database);
+    
+    sqlite3_exec(database, "BEGIN TRANSACTION", NULL, NULL, &errorMessage);
     
     for(Post *post in posts) {
-        [self addPostToTable:post];
+        [self addPostToTable:post database:database];
     }
-    sqlite3_exec(self.database, "COMMIT TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3_exec(database, "COMMIT TRANSACTION", NULL, NULL, &errorMessage);
+    sqlite3_close(database);
 }
 
 
@@ -262,28 +326,63 @@ static DatabaseManager *databaseManager = nil;
 
 # pragma mark - AuxillaryMethods
 
-- (BOOL) executeStatement:(NSString*)sqlQuery
+- (BOOL) executeStatement:(NSString*)sqlQuery database:(sqlite3*)database;
 {
     sqlite3_stmt *statement;
     
-    sqlite3_prepare_v2(_database, [sqlQuery UTF8String], -1, &statement, NULL);
-    if (sqlite3_step(statement) != SQLITE_DONE) {
-        printf("ERROR EXECUTING: %s\t%s\n", [sqlQuery UTF8String], sqlite3_errmsg(_database));
-        return false;
-    }
+    int errorCount = 0;
     
-    sqlite3_finalize(statement);
+    //In case the DB is busy on another thread
+    while(errorCount < MAX_DB_TRIES) {
+        
+        //The statement compiled correctly
+        if(sqlite3_prepare_v2(database, [sqlQuery UTF8String], -1, &statement, NULL) == SQLITE_OK) {
+            
+            //If it wasn't executed correctly
+            if (sqlite3_step(statement) != SQLITE_DONE) {
+                
+                //Keep on trying
+                errorCount++;
+                printf("ERROR EXECUTING: %s\t%s\n", [sqlQuery UTF8String], sqlite3_errmsg(database));
+            }
+            
+            //Statement successfully executed
+            else {
+                errorCount = MAX_DB_TRIES;
+            }
+            
+            //Clear up memory
+            sqlite3_finalize(statement);
+        }
+        
+        //Problem compiling statement
+        else {
+            
+            //Try again and clear up memory
+            errorCount++;
+            sqlite3_finalize(statement);
+            
+            if(errorCount == MAX_DB_TRIES) {
+                printf("MAX TRIES REACHED FOR: %s\t%s\n", [sqlQuery UTF8String], sqlite3_errmsg(database));
+                return false;
+            }
+            
+            //Pause for some time
+            else {
+                [NSThread sleepForTimeInterval:DB_SLEEP_FOR_TIME];
+            }
+        }
+    }
     
     return true;
 }
 
-- (void) createTable:(NSString*)createTableStatement tableName:(NSString*)tableName;
+- (void) createTable:(NSString*)createTableStatement tableName:(NSString*)tableName database:(sqlite3*)database;
 {
     const char *sqlStatement = [createTableStatement UTF8String];
-    
     char *errorMessage;
-    if(sqlite3_exec(_database, sqlStatement, NULL, NULL, &errorMessage) != SQLITE_OK) {
-        printf("ERROR CREATING table: %s\t%s\n", [tableName UTF8String], sqlite3_errmsg(_database));
+    if(sqlite3_exec(database, sqlStatement, NULL, NULL, &errorMessage) != SQLITE_OK) {
+        printf("ERROR CREATING table: %s\t%s\n", [tableName UTF8String], sqlite3_errmsg(database));
     }
     else {
         printf("Table created: %s\n", [tableName UTF8String]);
