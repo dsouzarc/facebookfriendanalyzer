95afb9b9943c0894460f21e5f7f3327dd6e263fe
Ryan D'souza <dsouzarc@gmail.com>
2015-08-04 00:53:30 -0400

    First commit

---
+++ Facebook Friend Analyzer/FacebookFriendsInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  FacebookFriendsInfoViewController.h
+//  Facebook Friend Analyzer
+//
+//  Created by Ryan D'souza on 8/4/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacebookFriendsInfoViewController : UIViewController
+
+@end
---
+++ Facebook Friend Analyzer/FacebookFriendsInfoViewController.m
@@ -0,0 +1,37 @@
+//
+//  FacebookFriendsInfoViewController.m
+//  Facebook Friend Analyzer
+//
+//  Created by Ryan D'souza on 8/4/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "FacebookFriendsInfoViewController.h"
+
+@interface FacebookFriendsInfoViewController ()
+
+@end
+
+@implementation FacebookFriendsInfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
